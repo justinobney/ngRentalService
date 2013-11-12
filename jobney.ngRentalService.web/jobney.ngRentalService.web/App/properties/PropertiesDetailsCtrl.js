203eaec7d41f22a7b0fa5aedc43182fcf19d1b57
Justin Obney <justinobney@gmail.com>
2013-11-11 22:36:14 -0600

    Adding PropertiesDetailsCtrl and offsetting map because panel is open

---
+++ jobney.ngRentalService.web/jobney.ngRentalService.web/App/properties/PropertiesDetailsCtrl.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    'use strict';
+
+    var app = angular.module('ngRentalService');
+
+    app.controller('PropertiesDetailsCtrl', ['$scope', 'MapService', 'LayoutManager',
+        function ($scope, MapService, LayoutManager) {
+            
+            activate();
+
+            function activate() {
+                offsetMap();
+            }
+
+            function offsetMap() {
+                var flyout = LayoutManager.panels.flyOut;
+                var map = LayoutManager.panels.maps;
+                var opts = {};
+
+                if (!flyout.state.isClosed) {
+                    var visibleWidth = map.pane.width() - flyout.pane.width();
+
+                    opts.offsetRight = visibleWidth / 2;
+                }
+
+                MapService.offsetMap(opts);
+            }
+        }]);
+})()
