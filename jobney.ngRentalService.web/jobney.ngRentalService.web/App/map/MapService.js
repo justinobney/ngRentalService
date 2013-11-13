c4447741aaf99106bd576a8888674083427b2586
Justin Obney <justinobney@gmail.com>
2013-11-12 23:02:21 -0600

    probably messing up my map service  :-|

--- jobney.ngRentalService.web/jobney.ngRentalService.web/App/map/MapService.js
+++ jobney.ngRentalService.web/jobney.ngRentalService.web/App/map/MapService.js
@@ -1,4 +1,4 @@
-﻿(function() {
+﻿(function () {
     'use strict';
 
     var app = angular.module('ngRentalService');
@@ -14,10 +14,50 @@
             var geoCache = {};
             var defaultAnimation = google.maps.Animation.DROP;
 
-            service.initMap = function (mapId, options) {
-                map = new google.maps.Map(document.getElementById(mapId), options);
+            service.openMarkerInfo = function (fnFindMarker) {
+                if (!myMarkers.length)
+                    return;
+
+                var currentMarker = _.where(myMarkers, fnFindMarker);
+
+                if (currentMarker.length == 0)
+                    return;
+
+                service.closeInfoWindows();
+                service.panTo(currentMarker[0].getPosition());
+                currentMarker[0].InfoBox.open(map, currentMarker[0]);
             };
-            
+
+            service.panTo = function (latLng) {
+                map.panTo(latLng);
+            };
+
+            service.plot = function (point, model, options) {
+                if (options.clearPrevious)
+                    service.clearMap();
+
+                var marker = service.createMarker(point, null, google.maps.Animation.DROP);
+
+                if (options.infoBoxTemplate && model)
+                    createInfoBox(marker, options, model);
+
+                myMarkers.push(marker);
+            };
+
+            service.plotPoints = function (points, options) {
+                if (options.clearPrevious)
+                    service.clearMap();
+
+                angular.forEach(points, function (value) {
+                    var marker = service.createMarker(value[0], null, google.maps.Animation.DROP);
+
+                    if (options.infoBoxTemplate && value[1])
+                        createInfoBox(marker, options, value[1]);
+
+                    myMarkers.push(marker);
+                });
+            };
+
             service.clearMap = function () {
                 service.closeInfoWindows();
                 angular.forEach(myMarkers, function (marker, key) {
@@ -25,11 +65,6 @@
                 });
                 myMarkers = [];
             };
-            
-            service.panTo = function (latLng) {
-                map.panTo(latLng);
-            };
-            
             service.offsetMap = function (options) {
                 if (options.offsetRight) {
                     Common.$timeout(function () {
@@ -48,10 +83,32 @@
                         map.panBy(0, options.offsetBottom);
                     }, 0);
                 }
+
+            service.closeInfoWindows = function () {
+                angular.forEach(myMarkers, function (marker, key) {
+                    if (marker.InfoBox)
+                        marker.InfoBox.close();
+                });
             };
 
             service.convertToGoogleLatLng = function (lat, lng) {
                 return new google.maps.LatLng(lat, lng);
+            };
+
+            service.createMarker = function (googleLatLng, icon, animation) {
+                var optMap = {
+                    map: map,
+                    position: googleLatLng
+                };
+                if (icon)
+                    optMap.icon = icon;
+
+                if (animation)
+                    optMap.animation = animation;
+
+                var marker = new google.maps.Marker(optMap);
+
+                return marker;
             };
 
             service.geocodeAddress = function (address) {
@@ -89,62 +146,7 @@
 
                 return deferred.promise;
             };
-            
-            service.createMarker = function (googleLatLng, icon, animation) {
-                var optMap = {
-                    map: map,
-                    position: googleLatLng
-                };
-                if (icon)
-                    optMap.icon = icon;
 
-                if (animation)
-                    optMap.animation = animation;
-
-                var marker = new google.maps.Marker(optMap);
-
-                return marker;
-            };
-
-            service.plotPoints = function (points, options) {
-                if (options.clearPrevious)
-                    service.clearMap();
-
-                angular.forEach(points, function (value) {
-                    var marker = service.createMarker(value[0], null, google.maps.Animation.DROP);
-
-                    if (options.infoBoxTemplate && value[1])
-                        createInfoBox(marker, options.infoBoxTemplate, value[1]);
-
-                    myMarkers.push(marker);
-                });
-
-                fitMarkerBoundsDebounced();
-            };
-
-            service.openMarkerInfo = function (fnFindMarker) {
-                if (!myMarkers.length)
-                    return;
-
-                var currentMarker = myMarkers.filter(function (marker) {
-                    return fnFindMarker(marker);
-                });
-
-                if (currentMarker.length == 0)
-                    return;
-
-                service.closeInfoWindows();
-                service.panTo(currentMarker[0].getPosition());
-                currentMarker[0].InfoBox.open(map, currentMarker[0]);
-            };
-            
-            service.closeInfoWindows = function () {
-                angular.forEach(myMarkers, function (marker, key) {
-                    if (marker.InfoBox)
-                        marker.InfoBox.close();
-                });
-            };
-            
             service.createPolygon = function (points) {
                 var invisColor = "#000000";
                 var outlineColor = "#0ABA02";
@@ -164,21 +166,27 @@
                 polygon.setMap(map);
             };
 
-            function createInfoBox(marker, infoBoxTemplate, model) {
+            service.initMap = function (mapId, options) {
+                map = new google.maps.Map(document.getElementById(mapId), options);
+            };
 
-                $http.get(infoBoxTemplate, {
+            function createInfoBox(marker, options, model) {
+
+                $http.get(options.infoBoxTemplate, {
                     cache: $templateCache
                 }).success(function (tplContent) {
                     var compiledTemplate = $interpolate(tplContent)(model);
+
+                    var offSet = (options.width) ? -1 * (options.width / 2) : -140;
 
                     var myOptions = {
                         content: compiledTemplate,
                         disableAutoPan: false,
                         maxWidth: 0,
-                        pixelOffset: new google.maps.Size(-140, 0),
+                        pixelOffset: new google.maps.Size(offSet, 0),
                         zIndex: null,
                         boxStyle: {
-                            width: "280px"
+                            width: (options.width) ? options.width + 'px' : "280px"
                         },
                         closeBoxMargin: "5px 5px 5px 0",
                         closeBoxURL: "", //http://cdn1.iconfinder.com/data/icons/mimiGlyphs/16/close_delete.png
