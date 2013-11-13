0b827cfdfa51109d83a15ea6d055b96797bee9bd
Justin Obney <justinobney@gmail.com>
2013-11-12 23:50:44 -0600

    Implementing google place search

--- jobney.ngRentalService.web/jobney.ngRentalService.web/App/properties/PropertiesNewCtrl.js
+++ jobney.ngRentalService.web/jobney.ngRentalService.web/App/properties/PropertiesNewCtrl.js
@@ -3,13 +3,46 @@
 
     var app = angular.module('ngRentalService');
 
-    app.controller('PropertiesNewCtrl', ['$scope', 'Common', 'LayoutManager',
-        function ($scope, Common, LayoutManager) {
+    app.controller('PropertiesNewCtrl', ['$scope', 'Common', 'MapService', 'LayoutManager',
+        function ($scope, Common, MapService, LayoutManager) {
             
             activate();
 
             function activate() {
+                $scope.newProperty = {
+                    address1: '',
+                    address2: '',
+                    city: '',
+                    state: '',
+                    zipcode: '',
+                    status: ''
+                };
                 
+                $scope.result = '';
+                $scope.options = {};
+                $scope.details = '';
+                
+                function watch_details() {
+                    return $scope.details;
+                }
+
+                $scope.$watch(watch_details, function (newVal, oldVal) {
+                    if (newVal) {
+                        var location = newVal.geometry.location;
+
+                        console.log(newVal);
+
+                        var place = MapService.convertPlaceResultToPlace(newVal);
+
+                        $scope.newProperty.address1 = place.streetNumber + ' ' + place.route;
+                        $scope.newProperty.city = place.locality;
+                        $scope.newProperty.state = place.state;
+                        $scope.newProperty.zipcode = place.postalCode;
+
+                        MapService.panTo(location);
+                        LayoutManager.handleInfoboxOffset();
+                    }
+                });
             }
         }]);
 })()
