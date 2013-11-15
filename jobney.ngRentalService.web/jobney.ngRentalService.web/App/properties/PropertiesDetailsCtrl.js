bb9a3fcc6bdc145a6b5a3513b2b0846f41718e81
Justin Obney <justinobney@gmail.com>
2013-11-14 20:30:34 -0600

    coding property details state

--- jobney.ngRentalService.web/jobney.ngRentalService.web/App/properties/PropertiesDetailsCtrl.js
+++ jobney.ngRentalService.web/jobney.ngRentalService.web/App/properties/PropertiesDetailsCtrl.js
@@ -3,13 +3,17 @@
 
     var app = angular.module('ngRentalService');
 
-    app.controller('PropertiesDetailsCtrl', ['$scope',
-        function ($scope) {
-            
+    app.controller('PropertiesDetailsCtrl', ['$scope', '$stateParams', 'PropertiesService',
+        function ($scope, $stateParams, PropertiesService) {
+
             activate();
 
             function activate() {
-                
+                var id = $stateParams.id;
+
+                PropertiesService.findById(id).then(function(property) {
+                    $scope.newProperty = property;
+                });
             }
         }]);
 })()
