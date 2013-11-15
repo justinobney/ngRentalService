e2a1bc8da1de74f1851fe930e977e6caa90f895d
Justin Obney <justinobney@gmail.com>
2013-11-14 21:20:41 -0600

    Property form updates

--- jobney.ngRentalService.web/jobney.ngRentalService.web/App/properties/PropertiesDetailsCtrl.js
+++ jobney.ngRentalService.web/jobney.ngRentalService.web/App/properties/PropertiesDetailsCtrl.js
@@ -3,8 +3,8 @@
 
     var app = angular.module('ngRentalService');
 
-    app.controller('PropertiesDetailsCtrl', ['$scope', '$stateParams', 'PropertiesService',
-        function ($scope, $stateParams, PropertiesService) {
+    app.controller('PropertiesDetailsCtrl', ['$scope', '$stateParams', 'PropertiesService', 'MapService',
+        function ($scope, $stateParams, PropertiesService, MapService) {
 
             activate();
 
@@ -13,7 +13,31 @@
 
                 PropertiesService.findById(id).then(function(property) {
                     $scope.newProperty = property;
+                    getAddressImage();
                 });
+            }
+
+            function getAddressImage() {
+                // geocode address
+                var address = [
+                    $scope.newProperty.address1,
+                    $scope.newProperty.address2,
+                    $scope.newProperty.city,
+                    $scope.newProperty.state,
+                    $scope.newProperty.zipcode
+                ].join(' ');
+
+                
+                var handleGeocodeComplete = function(location) {
+                    MapService.getStreetViewImage(location, 600, 400).then(handleStreetViewImageComplete);
+                };
+                
+                var handleStreetViewImageComplete = function(result) {
+                    if (result.success)
+                        $scope.locationStreetView = result.url;
+                };
+                
+                MapService.geocodeAddress(address).then(handleGeocodeComplete);
             }
         }]);
 })()
