55ca4c160ca9a54affbec017772b13150cf68618
Justin Obney <justinobney@gmail.com>
2013-11-16 21:56:50 -0600

    scaffolding out teants panel

---
+++ jobney.ngRentalService.web/jobney.ngRentalService.web/App/app.states.tenants.js
@@ -0,0 +1,37 @@
+﻿(function () {
+    "use strict";
+
+    var app = angular.module('ngRentalService');
+
+    app.config(['$stateProvider', function ($stateProvider) { //$urlRouterProvider
+        $stateProvider
+            .state('tenants', {
+                url: '/tenants',
+                views: {
+                    "panel": {
+                        templateUrl: "app/tenants/tenants.panel.html"
+                    }
+                },
+                data: {
+                    uiStateMap: {
+                        drawer: false,
+                        modal: false
+                    }
+                }
+            })
+            .state('tenants.details', {
+                url: '/:id',
+                views: {
+                    "drawer@": {
+                        templateUrl: "app/tenants/tenants.details.html"
+                    }
+                },
+                data: {
+                    uiStateMap: {
+                        drawer: true,
+                        modal: false
+                    }
+                }
+            });
+    }]);
+})();
